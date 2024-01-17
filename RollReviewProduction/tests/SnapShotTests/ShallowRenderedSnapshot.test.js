83c38c4780de5879eb6a139d12ecef84c8286f83
Charlie <cdb8987@gmail.com>
2024-01-17 11:21:46 -0500

    got handful of snapshot tests working.  However, I am running into issues with the contextx.  I made a mistake....I am trying to test it in the test file.  What you you need to do is test in a simple file that contains just the problem.  Get used to doing this.

--- RollReviewProduction/tests/SnapShotTests/ShallowRenderedSnapshot.test.js
+++ RollReviewProduction/tests/SnapShotTests/ShallowRenderedSnapshot.test.js
@@ -1,59 +1,90 @@
 import React from "react";
 import {act} from 'react-test-renderer';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import AddPositionInputText from '../../Components/Modals/AddPositionInputText.js'
 import AddTechniqueInputText from '../../Components/Modals/AddTechniqueInputText.js'
-import EventTaggingModal from '../../Components/Modals/EventTaggingModal.js'
+// import EventTaggingModal from '../../Components/Modals/EventTaggingModal.js'
 import InputDrillModal from '../../Components/Modals/InputDrillModal.js'
-import InputLocalVideoModal from '../../Components/Modals/InputLocalVideoModal.js'
-import InputRollModal from '../../Components/Modals/InputRollModal.js'
-import InputTutorialModal from '../../Components/Modals/InputTutorialModal.js'
-import ModalContainer from '../../Components/Modals/ModalContainer.js'
-import Notesinput from '../../Components/Modals/Notesinput.js'
-//       
+// import InputLocalVideoModal from '../../Components/Modals/InputLocalVideoModal.js'
+// import InputRollModal from '../../Components/Modals/InputRollModal.js'
+// import InputTutorialModal from '../../Components/Modals/InputTutorialModal.js'
+// import ModalContainer from '../../Components/Modals/ModalContainer.js'
+// import Notesinput from '../../Components/Modals/Notesinput.js'  
 
 
 import { Button}from 'react-native'
 import {TextInput} from 'react-native-paper'
+import { SetPositionsContext } from "../../Components/Contexts/SetPositionsContext.js";
+import { SetTechniquesContext } from "../../Components/Contexts/SetTechniquesContext.js";
+import { techniqueSelectionContext } from "../../Components/Contexts/techniqueSelectionContext.js";
+import { positionSelectionContext } from "../../Components/Contexts/positionSelectionContext.js";
 
 
-//Checks Shallow Rendered Snapshot for AddPositionInputText.js
-test('Shallow Render matches snapshot: AddPositionInputText',  () => {
-    const renderer = new ShallowRenderer()
-    const tree = renderer.render(<AddPositionInputText 
-    />);
-    
-    expect(tree).toMatchSnapshot();
-    
-});
 
-//Checks Shallow Rendered Snapshot for AddTechniqueInputText.js
-test('Shallow Render matches snapshot: AddTechniqueInputText',  () => {
-    const renderer = new ShallowRenderer()
-    const tree = renderer.render(<AddTechniqueInputText 
-    />);
-    
+jest.mock('./SetPositionsContext');
+jest.mock('./SetTechniquesContext');
+jest.mock('./techniqueSelectionContext');
+jest.mock('./positionSelectionContext');
 
-    expect(tree).toMatchSnapshot();
-    
-});
 
-//Checks Shallow Rendered Snapshot for EnterURLInputbox.js
-test('Shallow Render matches snapshot: EnterURLInputbox',  () => {
+
+
+// //Checks Shallow Rendered Snapshot for AddPositionInputText.js
+// test('Shallow Render matches snapshot: AddPositionInputText',  () => {
+//     const renderer = new ShallowRenderer()
+//     const tree = renderer.render(<AddPositionInputText 
+//     />);
     
-    const renderer = new ShallowRenderer()
-    const tree = renderer.render(<EventTaggingModal 
-    />);
+//     expect(tree).toMatchSnapshot();
+    
+// });
+
+// //Checks Shallow Rendered Snapshot for AddTechniqueInputText.js
+// test('Shallow Render matches snapshot: AddTechniqueInputText',  () => {
+//     const renderer = new ShallowRenderer()
+//     const tree = renderer.render(<AddTechniqueInputText 
+//     />);
     
 
-    expect(tree).toMatchSnapshot();
+//     expect(tree).toMatchSnapshot();
     
-});
+// });
+
+// //Checks Shallow Rendered Snapshot for EnterURLInputbox.js
+// test('Shallow Render matches snapshot: EnterURLInputbox',  () => {
+    
+//     const renderer = new ShallowRenderer()
+//     const tree = renderer.render(<EventTaggingModal 
+//     />);
+    
+
+//     expect(tree).toMatchSnapshot();
+    
+// });
 
 //Checks Shallow Rendered Snapshot for InputDrillModal.js
+
+
 test('Shallow Render matches snapshot: InputDrillModal',  () => {
     
+    SetPositionsContext.mockReturnValue({
+    setPositions: jest.fn(), 
+  });
+    SetTechniquesContext.mockReturnValue({
+    SetTechniquesContext: jest.fn(), 
+  });
+    techniqueSelectionContext.mockReturnValue({
+    techniqueSelectionContext: '', 
+    });
+
+    positionSelectionContext.mockReturnValue({
+    positionSelectionContext: '', 
+  });
+
+
+
+
+
     const renderer = new ShallowRenderer()
     const tree = renderer.render(<InputDrillModal 
     />);
@@ -63,62 +94,62 @@ test('Shallow Render matches snapshot: InputDrillModal',  () => {
     
 });
 
-//Checks Shallow Rendered Snapshot for InputLocalVideoModal.js
-test('Shallow Render matches snapshot: InputLocalVideoModal',  () => {
+// //Checks Shallow Rendered Snapshot for InputLocalVideoModal.js
+// test('Shallow Render matches snapshot: InputLocalVideoModal',  () => {
     
-    const renderer = new ShallowRenderer()
-    const tree = renderer.render(<InputLocalVideoModal 
-    />);
-    
-
-    expect(tree).toMatchSnapshot();
-    
-});
-
-//Checks Shallow Rendered Snapshot for InputRollModal.js
-test('Shallow Render matches snapshot: InputRollModal',  () => {
-    
-    const renderer = new ShallowRenderer()
-    const tree = renderer.render(<InputRollModal 
-    />);
+//     const renderer = new ShallowRenderer()
+//     const tree = renderer.render(<InputLocalVideoModal 
+//     />);
     
 
-    expect(tree).toMatchSnapshot();
+//     expect(tree).toMatchSnapshot();
     
-});
+// });
 
-//Checks Shallow Rendered Snapshot for InputTutorialModal.js
-test('Shallow Render matches snapshot: InputTutorialModal',  () => {
+// //Checks Shallow Rendered Snapshot for InputRollModal.js
+// test('Shallow Render matches snapshot: InputRollModal',  () => {
     
-    const renderer = new ShallowRenderer()
-    const tree = renderer.render(<InputTutorialModal 
-    />);
-    
-
-    expect(tree).toMatchSnapshot();
-    
-});
-
-//Checks Shallow Rendered Snapshot for ModalContainer.js
-test('Shallow Render matches snapshot: ModalContainer',  () => {
-    
-    const renderer = new ShallowRenderer()
-    const tree = renderer.render(<ModalContainer 
-    />);
+//     const renderer = new ShallowRenderer()
+//     const tree = renderer.render(<InputRollModal 
+//     />);
     
 
-    expect(tree).toMatchSnapshot();
+//     expect(tree).toMatchSnapshot();
     
-});
+// });
 
-//Checks Shallow Rendered Snapshot for Notesinput.js
-test('Shallow Render matches snapshot: Notesinput',  () => {
+// //Checks Shallow Rendered Snapshot for InputTutorialModal.js
+// test('Shallow Render matches snapshot: InputTutorialModal',  () => {
     
-    const renderer = new ShallowRenderer()
-    const tree = renderer.render(<Notesinput 
-    />);
+//     const renderer = new ShallowRenderer()
+//     const tree = renderer.render(<InputTutorialModal 
+//     />);
     
 
-    expect(tree).toMatchSnapshot();
+//     expect(tree).toMatchSnapshot();
     
-});
+// });
+
+// //Checks Shallow Rendered Snapshot for ModalContainer.js
+// test('Shallow Render matches snapshot: ModalContainer',  () => {
+    
+//     const renderer = new ShallowRenderer()
+//     const tree = renderer.render(<ModalContainer 
+//     />);
+    
+
+//     expect(tree).toMatchSnapshot();
+    
+// });
+
+// //Checks Shallow Rendered Snapshot for Notesinput.js
+// test('Shallow Render matches snapshot: Notesinput',  () => {
+    
+//     const renderer = new ShallowRenderer()
+//     const tree = renderer.render(<Notesinput 
+//     />);
+    
+
+//     expect(tree).toMatchSnapshot();
+    
+// });
